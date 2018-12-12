60ac064b59e22beb20fa6f9b7470f6296032d2a3
Mico Chu <angelomycar@gmail.com>
2018-12-12 11:55:28 +0000

    Merged in feature/biobank-case-date-fields (pull request #16)
    Feature Date Field For Case Specimen
    
    Approved-by: Mico Chu <angelomycar@gmail.com>

--- src/app/core/interfaces/specimenjson.ts
+++ src/app/core/interfaces/specimenjson.ts
@@ -4,6 +4,7 @@ export interface SpecimenJSON {
     qty: number;
     spec: string;
     spec_type: string;
+    collectedDate: string;
     characteristic: string;
     qty_avail: number;
     history: SpecimenHistoryJSON[];
