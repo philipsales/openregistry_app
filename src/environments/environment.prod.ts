0128213e5ff5e991ae48ac2106c170de4e87e836
philipsales <pj_sal87@yahoo.com>
2019-02-05 18:42:02 +0800

    Update environment parameters

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,14 +7,14 @@ export const environment = {
   production: true,
   API_QUESTION_HOST: 'http://localhost:8888',
   API_QUESTION_ENDPOINT: 'http://localhost:8888/default',
-  API_HOST: 'https://pgh.biobank.ph',
-  API_ENDPOINT: 'https://pgh.biobank.ph/',
-  API_ALLOWED_DOMAINS: ['biobank.ph:3000', 'pgh.biobank.ph'],
+  API_HOST: 'https://api.biobank.ph',
+  API_ENDPOINT: 'https://api.biobank.ph/',
+  API_ALLOWED_DOMAINS: ['app.biobank.ph:3000', 'api.biobank.ph'],
   ORG_BIOBANK: 'University of the Philippines Diliman',
   ORG_MEDICAL: 'University of the Philippines - Philippine General Hospital',
   DEPT_MEDICAL: 'General Surgery Department',
   DEPT_BIOBANK: 'Institute of Biology',
   FORM_TYPE_BIOBANK: 'Biobanking Repository',
   FORM_TYPE_MEDICAL: 'Patient Repository',
-  VERSION: 'v1.00.00-beta'
+  VERSION: 'v1.00.00'
 };
