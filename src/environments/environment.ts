11871256b75b160a320076c2241c1455ac30d81f
philipsales <pj_sal87@yahoo.com>
2019-02-05 18:56:33 +0800

    Update version number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,15 +8,13 @@ export const environment = {
     API_QUESTION_HOST: 'http://localhost:8888',
     API_QUESTION_ENDPOINT: 'http://localhost:8888/default',
     API_HOST: 'http://localhost:3001',
-    // API_HOST: 'http://kristhiantiu.com:3000',
     API_ENDPOINT: 'http://localhost:3001/',
-    // API_ENDPOINT: 'http://kristhiantiu.com:3000/',
-    API_ALLOWED_DOMAINS: ['localhost:3001', 'kristhiantiu.com:3000'],
+    API_ALLOWED_DOMAINS: ['localhost:3001', 'localhost:4200'],
     ORG_BIOBANK: 'University of the Philippines Diliman',
     ORG_MEDICAL: 'University of the Philippines - Philippine General Hospital',
     DEPT_MEDICAL: 'General Surgery Department',
     DEPT_BIOBANK: 'Institute of Biology',
     FORM_TYPE_BIOBANK: 'Biobanking Repository',
     FORM_TYPE_MEDICAL: 'Patient Repository',
-    VERSION: 'v01.00.00-alpha'
+    VERSION: 'v1.00.00'
 };
