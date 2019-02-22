70d88a831f17b1a9c235d609dd8fc55614a9be62
Mico Chu <angelomycar@gmail.com>
2019-02-22 04:26:49 +0000

    Merged in feature/biobank-medical-pagination (pull request #45)
    Feature/biobank medical pagination
    
    * format biobank list case html
    
    * add case service list function for pagination
    
    * add medical case module a working pagination
    
    * add medical biobank real search
    
    * add biobank case "case number" sorting
    
    * change medical case to support pagination, search, and sorting
    
    Approved-by: Mico Chu <angelomycar@gmail.com>

--- src/app/shared/_components/pcaricase-list/pcaricase-list.component.ts
+++ src/app/shared/_components/pcaricase-list/pcaricase-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Case } from 'app/core/models';
+import { CaseService } from 'app/core/services';
+import { MatPaginator } from '@angular/material';
+import { NoJWTError } from 'app/core/errors';
 
 @Component({
   selector: 'app-pcaricase-list',
@@ -12,19 +15,50 @@ export class PcaricaseListComponent implements OnInit {
   @Input() view_url: string;
   @Input() update_url: string;
   @Input() show_diagnosis: boolean;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  sort = 0; // desc == -1
+  pagelength:number;
+
   searchText = '';
   filter = '';
 
   is_adding_forms = true;
-  _cases: Case[];
-
-  @Input() set cases(value: Case[]) {
-    this._cases = value;
-  }// -- setter for forms
-
-  constructor() { }
+  cases: Case[];
+  constructor(private caseService: CaseService) { }
 
   ngOnInit() {
+    this.getCases({pageIndex: 0, pageSize: 10});
+  }
+
+
+  doSort() {
+    this.sort = this.sort == 1 ? -1 : 1;
+    this.getCases(this.paginator);
+  }
+
+  doSearch(newObject: any) {
+    let pageIndex = 0;
+    let pageSize = 10;
+    this.getCases({pageIndex, pageSize}, true);
+  }
+
+  getCases(paginator={
+    pageIndex: 0, 
+    pageSize: 10}, reset=false) {
+    this.caseService.list(paginator.pageIndex, 
+      paginator.pageSize,
+      this.searchText,
+      this.sort)
+      .subscribe(result => {
+        console.log(result, 'hello world');
+        this.cases = result.cases;
+        this.pagelength = result.count;
+        if (reset) {
+          this.paginator.pageIndex = 0;
+        }
+      })
   }
 
 }
